f3a019bb2babf1dc22a6a3312b9640db708ba995
imintsao <s3104029@gmail.com>
2022-02-21 14:43:26 +0800

    Refactoring Phase No.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,85 +9,128 @@ using UnityEngine;
 //transform scaling, make it pecisely, position exactly , bar with capsule, 
 public class GameManager : MonoBehaviour
 {
-    //float swingSpeed = 0.1f;
-    //GameObject sphereForTest;
-    GameObject rodTest;
-    GameObject hrBar;
-    GameObject ballTest;
-    Rigidbody rodRB;
-    Rigidbody hrBarRB;
-    HingeJoint rodHingeJoint;
-    FixedJoint ballFJ;
-    float refY;
-    float refYforBall;
+    GameObject[] rodArray;
+    int rodTotal;
+    Rigidbody tempRB;//this is declareing, so without "=" !!
     //int rodCount;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        rodTotal = 5;
+        rodArray = new GameObject[rodTotal];
+       
         //rodTest = new GameObject[rodCount];
 
         //rodCount = 3;
+        float xPos = 0;
+        float xGap = 1.5f;
+        //GameObject tempObject;
+        Transform tempTransform;
 
-        SetBar();
 
-        SetRod();
 
-        SetSphere();
+        for (int i = 0; i < rodTotal; i++)
+        {
+            tempTransform = SetBar(xPos); //Transform = Transform, why is that? ya why Transform?
 
-        
+            rodArray[i] = SetRod(tempTransform);
+
+            SetSphere(tempTransform, rodArray[i]);
+
+            xPos += xGap;
+
+        }
+
+
     }
 
-    private void SetSphere()
-    {
-        refYforBall = refY - rodTest.transform.localScale.y*2 - rodTest.transform.localScale.y / 4;
-        ballTest = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        ballTest.transform.position = new Vector3(hrBar.transform.position.x, refYforBall, hrBar.transform.position.z);
+    //private void SetSphere(GameObject hrBar)
+    private void SetSphere(Transform hrBar, GameObject refRod)
 
-        ballFJ = ballTest.AddComponent<FixedJoint>();
+    {
+        GameObject refBall;
+
+        Rigidbody rodRB;
+        FixedJoint ballFJ;
+        float refYforBall;
+        float refY;
+
+
+        refY = hrBar.transform.position.y - hrBar.transform.localScale.x / 2;
+
+        rodRB = refRod.GetComponent<Rigidbody>();
+
+        refYforBall = refY - refRod.transform.localScale.y * 2 - refRod.transform.localScale.y / 4;
+        refBall = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        refBall.transform.position = new Vector3(hrBar.transform.position.x, refYforBall, hrBar.transform.position.z);
+
+        ballFJ = refBall.AddComponent<FixedJoint>();
+
         ballFJ.connectedBody = rodRB;
     }
 
-    private void SetRod()
+    //private void SetRod(GameObject hrBar)
+    //private void SetRod(Transform hrBar)//this is the main focus//private GameObject SetRod
+    private GameObject SetRod (Transform hrBar)
+
     {
+        GameObject refRod;
+        Rigidbody rodRB;
+        Rigidbody hrBarRB;
+        HingeJoint rodHingeJoint;
+        float refY;
+
+
+
         refY = hrBar.transform.position.y - hrBar.transform.localScale.x / 2;
 
-        
-            //refY = hrBar.transform.position.y - hrBar.transform.localScale.y / 2; //first
-            //refY = hrBar.transform.position.y - hrBar.transform.localScale.x / 2;//when hrBar as cylinter rotate to 90"
+        //refY = hrBar.transform.position.y - hrBar.transform.localScale.y / 2; //first
+        //refY = hrBar.transform.position.y - hrBar.transform.localScale.x / 2;//when hrBar as cylinter rotate to 90"
 
 
-            rodTest= GameObject.CreatePrimitive(PrimitiveType.Capsule);
-            rodTest.transform.localScale = new Vector3(0.5f, 1.8f, 0.5f);
-            //rodForTest.transform.position = new Vector3(hrBar.transform.position.x, refY - rodForTest.transform.localScale.y, hrBar.transform.position.z);
-            rodTest.transform.position = new Vector3(hrBar.transform.position.x, refY - rodTest.transform.localScale.y, hrBar.transform.position.z);
-            //rodForTest.transform.position = new Vector3(hrBar.transform.position.x, hrBar.transform.position.y, hrBar.transform.position.z);
+        refRod = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        refRod.transform.localScale = new Vector3(0.5f, 1.8f, 0.5f);
+        //rodForTest.transform.position = new Vector3(hrBar.transform.position.x, refY - rodForTest.transform.localScale.y, hrBar.transform.position.z);
+        refRod.transform.position = new Vector3(hrBar.transform.position.x, refY - refRod.transform.localScale.y, hrBar.transform.position.z);
+        //rodForTest.transform.position = new Vector3(hrBar.transform.position.x, hrBar.transform.position.y, hrBar.transform.position.z);
+
+        //localScaleY is twice length for cyclinder and cpsusle,
 
 
-            //localScaleY is twice length for cyclinder and cpsusle,
+        rodHingeJoint = refRod.AddComponent<HingeJoint>();
+        hrBarRB = hrBar.GetComponent<Rigidbody>();
 
-            //rodForTest.transform.Rotate(40.0f, 0.0f, 0.0f, Space.Self);
+        rodHingeJoint.connectedBody = hrBarRB;
+        rodHingeJoint.axis = new Vector3(0f, 0f, 1f);
+        //rodHingeJoint.autoConfigureConnectedAnchor = (false);
 
-            rodHingeJoint = rodTest.AddComponent<HingeJoint>();
-            rodHingeJoint.connectedBody = hrBarRB;
-            //rodHingeJoint.autoConfigureConnectedAnchor = (false);
+        rodRB = refRod.GetComponent<Rigidbody>();
+        rodRB.useGravity = (false);//turn it on, then it can swin
+        rodRB.isKinematic = (true);//could be a trigger when "false" is like real physics
 
-            rodRB = rodTest.GetComponent<Rigidbody>();
-            rodRB.useGravity = (false);
-            rodRB.isKinematic = (true);//could be a trigger when "false" is like real physics
-        
+        return refRod;
+
     }
 
-    private void SetBar()
+    private Transform SetBar(float xPos) //still don't really know why use Transform class instead
+    //private GameObject SetBar(float xPos)
     {
+        GameObject hrBar;
+        Rigidbody hrBarRB;
+
+        //Transform hrBar;
+
         hrBar = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
         hrBar.transform.localScale = new Vector3(1f, 4f, 1f);
         hrBar.transform.Rotate(90f, 0f, 0f, Space.Self);
-        hrBar.transform.position = new Vector3(0f, 20f, 0.5f);
+        hrBar.transform.position = new Vector3(xPos, 20f, 0.5f);
         hrBarRB = hrBar.AddComponent<Rigidbody>();
         hrBarRB.useGravity = (false);
         hrBarRB.isKinematic = (true);
+
+        return hrBar.transform;
 
     }
 
@@ -96,12 +139,21 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyUp("s"))
         {
-            rodRB.useGravity = (true);
+            for (int i = 0; i<rodTotal; i++)
+            {
+                tempRB = rodArray[i].GetComponent<Rigidbody>();//it is defining, and use "="!!
+                tempRB.useGravity = (true);
+            }
         }
 
         if (Input.GetKeyUp("k"))
         {
-            rodRB.isKinematic = (false);
+            for (int i = 0; i < rodTotal; i++)
+            {
+                tempRB = rodArray[i].GetComponent<Rigidbody>();//it is defining, and use "="!!
+                tempRB.isKinematic = (false);
+            }
+            //rodRB.isKinematic = (false);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
